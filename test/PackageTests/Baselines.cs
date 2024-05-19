761ea70d386325510e30a296eb526e4cba91bfc0
Matt Kotsenas <matt.kotsenas@gmail.com>
2024-05-19 10:30:02 -0700

    Switch to MSTest

--- test/PackageTests/Baselines.cs
+++ test/PackageTests/Baselines.cs
@@ -1,24 +1,17 @@
 using System.Reflection;
 
-using Xunit.Abstractions;
-
 namespace GetPackFromProject.PackageTests;
 
-public class Baselines
+[TestClass]
+[UsesVerify]
+public partial class Baselines
 {
     private static readonly VerifySettings VerifySettings = new VerifySettings().ScrubNuspec();
-    private readonly ITestOutputHelper _logger;
 
-    public Baselines(ITestOutputHelper logger)
-    {
-        _logger = logger;
-    }
-
-    [Fact]
+    [TestMethod]
     public Task Match()
     {
         DirectoryInfo workingDirectory = GetWorkingDirectory();
-        _logger.WriteLine($"Enumerating files:{string.Join("\n\t", workingDirectory.GetFiles().Select(f => f.FullName))}");
 
         FileInfo package = workingDirectory.GetFiles("GetPackFromProject.*.nupkg").OrderByDescending(f => f.LastWriteTimeUtc).First();
 
@@ -28,8 +21,6 @@ public class Baselines
     private DirectoryInfo GetWorkingDirectory()
     {
         string location = Assembly.GetExecutingAssembly().Location;
-
-        _logger.WriteLine($"Using assembly location: {location}");
 
         return new FileInfo(location).Directory ?? throw new InvalidOperationException("Could not find the directory of the current assembly.");
     }
